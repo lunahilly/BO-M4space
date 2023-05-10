11415be612e2dd676a5c887d4dcc5cca3eaa1798
lunahilly <35861@ma-web.nl>
2023-05-10 10:08:37 +0200

    background music

--- Project B/Assets/Scripts/MovementScript.cs
+++ Project B/Assets/Scripts/MovementScript.cs
@@ -14,21 +14,25 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //naar links als pijltje word ingedrukt
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.MovePosition(transform.position + Vector3.left * speed * Time.deltaTime);
         }
 
+        //naar rechts als pijltje word ingedrukt
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.MovePosition(transform.position + Vector3.right * speed * Time.deltaTime);
         }
 
+        //naar links als A wordt ingedrukt
         if (Input.GetKey(KeyCode.A))
         {
             rb.MovePosition(transform.position + Vector3.left * speed * Time.deltaTime);
         }
 
+        //naar rechts als D wordt ingedrukt
         if (Input.GetKey(KeyCode.D))
         {
             rb.MovePosition(transform.position + Vector3.right * speed * Time.deltaTime);
