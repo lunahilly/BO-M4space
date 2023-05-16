7ced5bd804291de2b73844db7d3e517177b9874e
lunahilly <35861@ma-web.nl>
2023-05-16 11:39:05 +0200

    platform script

---
+++ Project B/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] GameObject[] waypoints;
+    int currentWaypointIndex = 0;
+    [SerializeField] float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moving();
+    }
+
+    // Update is called once per frame
+    public void moving()
+    {
+        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].transform.position) < 0.2f)
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, speed * Time.deltaTime);
+    }
+}
