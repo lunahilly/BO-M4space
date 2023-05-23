e6cf26808a7236996848025cab5796c5e2bc43eb
lunahilly <35861@ma-web.nl>
2023-05-23 09:51:52 +0200

    hearts added

--- Project B/Assets/Scripts/DeathCollide.cs
+++ Project B/Assets/Scripts/DeathCollide.cs
@@ -7,6 +7,9 @@ public class DeathCollide : MonoBehaviour
 {
     public int Lives = 3;
     public GameObject Player;
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +27,17 @@ public class DeathCollide : MonoBehaviour
     {
         if (Lives == 0)
         {
+            Destroy(heart1);
             Destroy(Player);
             SceneManager.LoadScene("Death");
+        }
+        if (Lives == 2)
+        {
+            Destroy(heart3);
+        }
+        if (Lives == 1)
+        {
+            Destroy(heart2);
         }
     }
 }
