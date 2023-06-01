be1900b8e22d067b83cea11f7ee616f1bf27470f
Teffer31 <t.barmak7@gmail.com>
2023-06-01 10:54:58 +0200

    melee not finished

--- Project B/Assets/Scripts/PlayerCombat.cs
+++ Project B/Assets/Scripts/PlayerCombat.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+
+    public Animator animator;
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    public float attackRange = 0.5f;
+    public int attackDamage = 40;
+
     // Update is called once per frame
     void Update()
     {
@@ -15,7 +23,21 @@ public class PlayerCombat : MonoBehaviour
 
     void Attack()
     {
+        animator.SetTrigger("Attack");
 
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<EnemyMelee>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+         return; 
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 
 }
