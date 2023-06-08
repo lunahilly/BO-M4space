cbb0910913fa37b846d64668a335457d7b4f06f2
lunahilly <35861@ma-web.nl>
2023-06-08 12:47:22 +0200

    chase enemy

--- Project B/Assets/Scripts/Enemy scripts/Chase.cs
+++ Project B/Assets/Scripts/Enemy scripts/Chase.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class Chase : MonoBehaviour
 {
+    [SerializeField] GameObject player;
+    [SerializeField] private float speed = 5f;
 
+    private void Update()
+    {
+        Vector2 distance = player.transform.position - this.transform.position;
+        Vector3 direction = distance.normalized;
+
+
+        transform.position += direction * speed * Time.deltaTime;
+        //transform.LookAt(player.transform.position);
+    }
 
 }
 
