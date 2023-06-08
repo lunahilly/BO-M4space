5f55ca785d7b7a11e9ab1d2d67d968abf04f4c87
lunahilly <35861@ma-web.nl>
2023-06-08 11:57:45 +0200

    ...

--- Project B/Assets/Scripts/Enemy scripts/Chase.cs
+++ Project B/Assets/Scripts/Enemy scripts/Chase.cs
@@ -4,34 +4,7 @@ using UnityEngine;
 
 public class Chase : MonoBehaviour
 {
-    Transform target;
-    Transform enemyTransform;
-    public float speed = 3f;
-    public float rotationSpeed = 3f;
 
-
-    void Start()
-    {
-        //obtain the game object Transform
-        enemyTransform = this.GetComponent<Transform>();
-    }
-
-    void Update()
-    {
-
-        target = GameObject.FindWithTag("Player").transform;
-        Vector3 targetHeading = target.position - transform.position;
-        Vector3 targetDirection = targetHeading.normalized;
-
-        //rotate to look at the player
-
-        transform.rotation = Quaternion.LookRotation(targetDirection); // Converts target direction vector to Quaternion
-
-
-        //move towards the player
-        enemyTransform.position += enemyTransform.forward * speed * Time.deltaTime;
-
-    }
 
 }
 
