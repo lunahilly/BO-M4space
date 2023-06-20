5c6d00ef2a8cb5408ac66d80fc51b571038be602
Teffer31 <t.barmak7@gmail.com>
2023-06-20 09:50:12 +0200

    test

--- Project B/Assets/Scripts/Player/melee.cs
+++ Project B/Assets/Scripts/Player/melee.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class melee : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float rotationSpeed = 100f;
+    private Camera mainCam;
+    private Vector3 mousePos;
+
     public static int damage = 15;
     public Animator m_Animator;
 
+    // Start is called before the first frame update
     void Start()
     {
         //Get the Animator attached to the GameObject you are intending to animate.
         m_Animator = gameObject.GetComponent<Animator>();
+
+        mainCam = Camera.main;
     }
 
     void Update()
@@ -25,6 +31,8 @@ public class melee : MonoBehaviour
         {
             m_Animator.SetTrigger("idle");
         }
+
+        RotateWithMouse();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -34,5 +42,14 @@ public class melee : MonoBehaviour
             Goober enemy = collision.GetComponent<Goober>();
             enemy.HandleHit(damage);
         }
+    }
+
+    private void RotateWithMouse()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
 }
