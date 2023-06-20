d48e75652ec56cbf682af88bcc6d8dcc8642ee82
Teffer31 <t.barmak7@gmail.com>
2023-06-20 13:11:30 +0200

    new loot scripts (not finished)

---
+++ Project B/Assets/Scripts/LootBag.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootBag : MonoBehaviour
+{
+    public GameObject droppedItemPrefab;
+    public List<Loot> lootList = new List<Loot>();
+
+    Loot GetDroppedItem()
+    {
+        int randomNumber = Random.Range(1, 101); // 1-100
+        List<Loot> possibleItems = new List<Loot>();
+        foreach (Loot item in lootList)
+        {
+            if(randomNumber <= item.dropChance)
+            {
+                possibleItems.Add(item);
+            }
+        }
+        if(possibleItems.Count > 0)
+        {
+            Loot droppedItem = possibleItems[Random.Range(0, possibleItems.Count)];
+        }
+    }
+}
